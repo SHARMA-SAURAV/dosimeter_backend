c5e1fa40a7d4cab8520bae4d0f9e0276268fcae6
Saurav_Sharma <sauravsh0010@gmail.com>
2025-07-31 09:56:16 +0530

    commit till update login and register

--- src/main/java/com/cdac/dosimeter_visualization/dto/DosimeterReadingRequestDTO.java
+++ src/main/java/com/cdac/dosimeter_visualization/dto/DosimeterReadingRequestDTO.java
@@ -1,5 +1,13 @@
 package com.cdac.dosimeter_visualization.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DosimeterReadingRequestDTO {
     private String deviceId;
     private String cpm;
